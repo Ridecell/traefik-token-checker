7ff286782d71ec0aa84734c3df1fdbf429562622
ajinkyak423 <ajinkyakumbhar423@gmail.com>
2025-04-21 13:47:38 +0530

    debug redis connection

--- redispool/redis.go
+++ redispool/redis.go
@@ -25,6 +25,7 @@ type pooledConn struct {
 }
 
 func New(redisURL string, poolSize int, idleTimeout time.Duration, logger *log.Logger) (*Pool, error) {
+	log.Printf("Redis URL being parsed: %s", redisURL)
 	u, err := url.Parse(redisURL)
 	if err != nil || (u.Scheme != "rediss") {
 		return nil, fmt.Errorf("invalid Redis URL")
